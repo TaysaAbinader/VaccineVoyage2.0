c675eb52f8a1efe39296f03c830c21cd24415887
tamseelaa <tamseelm@metropolia.fi>
2024-12-04 12:59:07 +0200

    web design

--- frontend/html/Navigation.js
+++ frontend/html/Navigation.js
@@ -5,7 +5,7 @@ const map = new mapboxgl.Map({
   container: 'map', // container ID
   //center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 1.5 // starting zoom
-  });
+  });//hehe
 
 
 
